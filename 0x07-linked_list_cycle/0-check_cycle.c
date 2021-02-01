e318d67e6723bc543ccdc7f0aefffb47097842e7
paisap <santiagoaldana58@gmail.com>
2021-02-01 07:16:35 -0500

    easy

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* check_cycle - prints all elements of a listint_t list
+* @list: pointer to head of list
+* Return: 0 or 1
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *aux, *aux1;
+
+	aux = list;
+	aux1 = list->next;
+	while (aux1->next && aux1->next->next)
+	{
+		if (aux1 > aux || aux1 == aux)
+			return (1);
+		aux = aux->next;
+		aux1 = aux1->next->next;
+	}
+	return (0);
+}
---
+++ 0x07-linked_list_cycle/README.md
@@ -0,0 +1,6 @@
+0. Linked list cycle 
+Technical interview preparation:
+
+You are not allowed to google anything
+Whiteboard first
+This task and all future technical interview prep tasks will include checks for the efficiency of your solution, i.e. is your solution’s runtime fast enough, does your solution require extra memory usage / mallocs, etc.
