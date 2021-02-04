b42544a8c04e2c2f98632b6551ca96ac1c9a70f0
paisap <santiagoaldana58@gmail.com>
2021-02-03 21:10:08 -0500

    null

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -11,14 +11,16 @@ int check_cycle(listint_t *list)
 {
 	listint_t *aux, *aux1;
 
+	if (list == NULL)
+		return (0);
 	aux = list;
 	aux1 = list->next;
 	while (aux1->next && aux1->next->next)
 	{
-      	if (aux1 > aux || aux1 == aux)
-      		return (1);
-      	aux = aux->next;
-      	aux1 = aux1->next->next;
+		if (aux1 > aux || aux1 == aux)
+			return (1);
+		aux = aux->next;
+		aux1 = aux1->next->next;
 	}
 	return (0);
 }
