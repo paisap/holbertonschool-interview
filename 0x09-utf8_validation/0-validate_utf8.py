8736b394683206dcda2df94cd6980347b78f7470
paisap <santiagoaldana58@gmail.com>
2021-02-17 16:39:57 -0500

    utf8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" valid UTF8 """
+
+
+def validUTF8(data):
+    """ :# """
+
+    n_bytes = 0
+    for num in data:
+
+        bin_rep = format(num, '#010b')[-8:]
+        print(bin_rep)
+        if n_bytes == 0:
+            for bit in bin_rep:
+
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+
+            if not(bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+
+        n_bytes -= 1
+
+    return n_bytes == 0
