cce0c18facda25c1099828b11e06cb8b27a85bfb
paisap <santiagoaldana58@gmail.com>
2021-06-24 23:33:18 -0500

    finish

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-""" valid UTF8 """
+"""validUTF8 Funnbytesion"""
 
 
 def validUTF8(data):
-    """ :# """
+    """method that determines if a given data set
+    represents a valid UTF-8 encoding"""
+    nbytes = 0
 
-    n_bytes = 0
-    for num in data:
+    m1 = 1 << 7
+    m2 = 1 << 6
 
-        bin_rep = format(num, '#010b')[-8:]
-        print(bin_rep)
-        if n_bytes == 0:
-            for bit in bin_rep:
-
-                if bit == '0':
-                    break
-                n_bytes += 1
-
-            if n_bytes == 0:
+    for i in data:
+        m = 1 << 7
+        if nbytes == 0:
+            while m & i:
+                nbytes += 1
+                m = m >> 1
+            if nbytes == 0:
                 continue
-
-            if n_bytes == 1 or n_bytes > 4:
+            if nbytes == 1 or nbytes > 4:
                 return False
         else:
-
-            if not(bin_rep[0] == '1' and bin_rep[1] == '0'):
+            if not (i & m1 and not (i & m2)):
                 return False
+        nbytes -= 1
+    return nbytes == 0
 
-        n_bytes -= 1
-
-    return n_bytes == 0
